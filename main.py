8cf7d1fb447ad8f0e594e5de7650cc41375f88c1
NotLinxed <empty>
2022-09-21 20:27:57 +0200

    Refactored the Poopz Discord Bot to use the new discord.py 2.0.1 package.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import os
 import discord
-import mcping
+import os
 
-from modules.profanity import Profanity
-from static.rooms import ChatRooms
 from dotenv import load_dotenv
+from poopz import PoopzClient
 
 # Ensure Environmental variables are set
 load_dotenv()
@@ -12,69 +10,7 @@ assert os.getenv('CLIENT_SECRET')
 assert os.getenv('MC_SERVER_IP')
 assert os.getenv('MC_SERVER_PORT')
 
-# Create and Initialize() a discord Client
-client = discord.Client()
-
-# Initialize profanity module
-profanity_module = Profanity()
-
-
-# If an on_ready() event is called, say we're ready
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-
-# If a on_message() event is called, figure out what to do
-@client.event
-async def on_message(message):
-
-    # If the person who sent the message is us
-    if message.author == client.user:
-        return  # Return nothing. Ignore it.
-
-    profanity_module\
-        .reset()\
-        .set_message(message)\
-        .check()
-
-    if profanity_module.has_profane_words:
-        await message.delete()
-        await message.author.send(profanity_module.get_message_reply())
-
-    # If it was a message sent in the #suggestions room add reactions to it
-    if message.channel.id == ChatRooms.SUGGESTIONS.value:
-        await message.add_reaction('\N{THUMBS UP SIGN}')
-        await message.add_reaction('\N{THUMBS DOWN SIGN}')
-
-    # If a message is sent to our ingame channel
-    if message.channel.id == ChatRooms.INGAME.value:
-
-        # If the message starts with !online or !list
-        if message.content.startswith('!online') or message.content.startswith('!list'):
-
-            # Ping the server to query player info
-            ping = mcping.ping(os.getenv('MC_SERVER_IP'), int(os.getenv('MC_SERVER_PORT')))
-
-            # Change the <Players> to <Strings>
-            names = [player.name for player in ping.players]
-
-            # Create an embed message and send it
-            embedded = discord.Embed(
-                title="Online Players - Mc.Popzi.Net",
-                description="\n".join(names), color=0x00ff00
-            )
-            await message.channel.send(embed=embedded)
-
-    # If a message is sent to our screenshots channel
-    if message.channel.id == ChatRooms.SCREENSHOTS.value:
-        # If it's not got any screenshots in it
-        if not message.attachments:
-            await message.delete()
-            await message.author.send(
-                'Please keep this channel to screenshots only, or I will hunt you down ' +
-                'and feast on your body. \N{POULTRY LEG}\N{FORK AND KNIFE}'
-            )
-
-# Login & Run our client using our secret password
+intents = discord.Intents.default()
+intents.message_content = True
+client = PoopzClient(intents=intents)
 client.run(os.getenv('CLIENT_SECRET'))
---
+++ poopz.py
@@ -0,0 +1,68 @@
+from typing import Any
+
+import os
+import mcping
+import discord
+from discord import Intents
+
+from modules.profanity import Profanity
+from static.rooms import ChatRooms
+
+
+class PoopzClient(discord.Client):
+    def __init__(self, *, intents: Intents, **options: Any):
+        super().__init__(intents=intents, **options)
+        self.profanity_module = Profanity()
+
+    async def on_ready(self):
+        print(f'We have logged on as {self.user}!')
+
+    async def on_message(self, message):
+        # If the person who sent the message is us
+        if message.author == self.user:
+            return  # Return nothing. Ignore it.
+
+        self.profanity_module\
+            .reset()\
+            .set_message(message)\
+            .check()
+
+        if self.profanity_module.has_profane_words:
+            await message.delete()
+            await message.author.send(self.profanity_module.get_message_reply())
+            # Can return here, nothing else to do with the message.
+            return
+
+        # If it was a message sent in the #suggestions room add reactions to it
+        if message.channel.id == ChatRooms.SUGGESTIONS.value:
+            await message.add_reaction('\N{THUMBS UP SIGN}')
+            await message.add_reaction('\N{THUMBS DOWN SIGN}')
+
+            # If a message is sent to our ingame channel
+            if message.channel.id == ChatRooms.INGAME.value:
+
+                # If the message starts with !online or !list
+                if message.content.startswith('!online') or message.content.startswith('!list'):
+                    # Ping the server to query player info
+                    ping = mcping.ping(os.getenv('MC_SERVER_IP'), int(os.getenv('MC_SERVER_PORT')))
+
+                    # Change the <Players> to <Strings>
+                    names = [player.name for player in ping.players]
+
+                    # Create an embed message and send it
+                    embedded = discord.Embed(
+                        title="Online Players - Mc.Popzi.Net",
+                        description="\n".join(names), color=0x00ff00
+                    )
+                    await message.channel.send(embed=embedded)
+
+        # If a message is sent to our screenshots channel
+        if message.channel.id == ChatRooms.SCREENSHOTS.value:
+            # If it's not got any screenshots in it
+            if not message.attachments:
+                await message.delete()
+                await message.author.send(
+                    'Please keep this channel to screenshots only, or I will hunt you down ' +
+                    'and feast on your body. \N{POULTRY LEG}\N{FORK AND KNIFE}'
+                )
+
