4a134dd470d9ee378dd6c1dd76be3b37496a8773
NotLinxed <empty>
2022-10-18 22:26:40 +0200

    Added: Task handler

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ assert os.getenv('MC_SERVER_IP')
 assert os.getenv('MC_SERVER_PORT')
 
 PoopzClient().run(os.getenv('CLIENT_SECRET'))
+
+#
+# def runnable_method(one_arg):
+#     print(f'Hello, {one_arg}')
+#
+#
+# task_module = TaskModule("tasks.yaml")
+# task_module.add_runnable('run', runnable_method)
+#
+# task_module.add_task(Task(
+#     'test',
+#     'run',
+#     datetime.datetime.now(),
+#     json.dumps(['World'])
+# ))
+#
+# task_module.start()
+
