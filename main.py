3df5a65e56938fce95bc944f58ce93a19ac7c69c
NotLinxed <empty>
2022-10-20 21:03:00 +0200

    Changed how Poopz muted members. It now uses Timeout

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import json
 import os
-import datetime
-from datetime import timedelta
 
 from dotenv import load_dotenv
 
-from modules.tasks import TaskModule, Task
 from poopz import PoopzClient
 
 # Ensure Environmental variables are set
@@ -19,25 +15,5 @@ def runnable_method(one_arg):
     print(f'Hello, {one_arg}')
 
 
-task_module = TaskModule("tasks.yaml")
-task_module.add_runnable('run', runnable_method)
-
-# Runs in one minute from now
-task_module.add_task(Task(
-    'test 1',
-    'run',
-    datetime.datetime.now() + timedelta(minutes=1),
-    json.dumps(['World 2'])
-))
-
-# Runs right away, as it should already have been run
-task_module.add_task(Task(
-    'test 2',
-    'run',
-    datetime.datetime.now() - timedelta(minutes=1),
-    json.dumps(['World 1'])
-))
-
-task_module.start()
-
-PoopzClient().run(os.getenv('CLIENT_SECRET'))
+poopz = PoopzClient()
+poopz.run(os.getenv('CLIENT_SECRET'))
